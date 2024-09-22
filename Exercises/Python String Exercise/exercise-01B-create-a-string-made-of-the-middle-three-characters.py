c1e327b99134b2c2822b084084bc4298427a71d8
Rodin Opuz <opuzrodin@gmail.com>
2024-09-22 12:11:14 +0100

    Add files via upload

---
+++ Exercises/Python String Exercise/exercise-01B-create-a-string-made-of-the-middle-three-characters.py
@@ -0,0 +1,8 @@
+str1 = "JhonDipPeta"
+str2 = "JaSonAy"
+
+len1 = len(str1) // 2
+len2 = len(str2) // 2
+
+print(str1[len1 - 1] + str1[len1] + str1[len1 + 1])
+print(str2[len2 - 1] + str2[len2] + str2[len2 + 1])
